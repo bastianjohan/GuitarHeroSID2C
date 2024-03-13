b850487824fbf83c831235e38cca48c457c794ce
Bastian Johan <132752475+bastianjohan@users.noreply.github.com>
2024-03-13 19:32:13 +0100

    Init + basic mechanics
    Circle move mechanics

---
+++ sketch.js
@@ -0,0 +1,57 @@
+let arrayCircles = [];
+let speedCircles = 0.01;
+let maxRadius = 300;
+
+class Circle {
+  constructor(t, a, r){
+    this.t = t;
+    this.angle = a;
+    this.radius = r;
+    this.x = 0;
+    this.y = 0;
+  }
+  update(cX, cY){
+    let lerpedRadius = lerp(0, maxRadius, this.t);
+    this.x = cX + lerpedRadius * cos(this.angle);
+    this.y = cY + lerpedRadius * sin(this.angle);
+    this.t += speedCircles;
+  }
+  draw(){
+    circle(this.x, this.y, this.radius);
+  }
+}
+
+function setup() {
+  createCanvas(innerWidth,innerHeight);
+  noFill();
+  strokeWeight(3);
+  stroke(255);
+}
+
+function update(){
+  for(let i = arrayCircles.length - 1; i >= 0; i--) {
+    let obj = arrayCircles[i];
+    obj.update(innerWidth/2, innerHeight/2);
+    if (obj.t >= 1) {
+      arrayCircles.splice(i, 1);
+    }
+  }
+}
+
+function draw() {
+  update();
+  background(0);
+  circle(innerWidth / 2, innerHeight / 2, 600);
+  circle(innerWidth / 2, innerHeight / 2, 10);
+  for(obj of arrayCircles){
+    obj.draw();
+  }
+}
+
+function keyPressed(){
+  if (keyCode === 32) {
+    let angle = random(0, 360);
+    console.log(angle);
+    arrayCircles.push(new Circle(0, angle, 50));
+  }
+}
